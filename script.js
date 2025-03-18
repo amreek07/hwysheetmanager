3708dcca70c157110ebd6c017af66fd802b4b3db
amreek07 <164839260+amreek07@users.noreply.github.com>
2025-03-18 14:40:04 +0530

    Ta Petro function creation

--- script.js
+++ script.js
@@ -2,28 +2,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const file = document.querySelector("#sheet");
   const submitBtn = document.querySelector("#submitBtn");
+  
 //   const downloadCSVBtn = document.querySelector('#downloadCSVBtn');
   let jsonData;
 
-  file.addEventListener("change", (event) => {
-    // console.log(event.target.files[0]);
-    const uploadedFile = event.target.files[0];
 
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const data = new Uint8Array(e.target.result);
-      const workbook = XLSX.read(data, { type: "array" });
+  // fileSelectorInput.forEach((file)=>{
+    file.addEventListener("change", (event) => {
+      // console.log(event.target.files[0]);
+      const uploadedFile = event.target.files[0];
+  
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: "array" });
+  
+        const sheetName = workbook.SheetNames[0]; // Get first sheet
+        const sheet = workbook.Sheets[sheetName];
+  
+        jsonData = XLSX.utils.sheet_to_json(sheet); // Convert sheet to JSON
+        // console.log(jsonData);
+        
+      };
+  
+      reader.readAsArrayBuffer(uploadedFile); // Read the file
+    });
+  // })
 
-      const sheetName = workbook.SheetNames[0]; // Get first sheet
-      const sheet = workbook.Sheets[sheetName];
-
-      jsonData = XLSX.utils.sheet_to_json(sheet); // Convert sheet to JSON
-      
-    };
-
-    reader.readAsArrayBuffer(uploadedFile); // Read the file
-  });
-
+//ambest sheet calculations
   const ambestCalculation = (e) => {
     // console.log();
     const sheetSaving = e.saving;
@@ -35,17 +41,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const cleanName = e.ts_name.replace(/'/g, ""); //remove ' from the name of travelcenters
 
     return {
-      travelCenter: cleanName,
+      travelcenter: cleanName,
       merchant: e.xts_id,
       price: sheetRetail.toFixed(3),
-      state: e.ts_state,
       difference: difference,
+      state: e.ts_state,
       discount: discount,
     };
-
-    // console.log(`travel center : ${e.ts_name}, Merchant: ${e.xts_id}, Price: ${sheetRetail} state: ${e.ts_state}, difference : ${difference}, discount: ${discount}`)
   };
 
+  //ambest data
   const ambest = () => {
     let newAmbestSheet = [];
     jsonData.forEach((data) => {
@@ -56,8 +61,51 @@ document.addEventListener("DOMContentLoaded", () => {
     downloadCSV(newAmbestSheet, "Ambest");
   };
 
+//ta petro data sheet functionality
+
+const taPetroFile = document.querySelector('#taSheet');
+
+let taJsonData;
+
+taPetroFile.addEventListener("change", (event) => {
+  // console.log(event.target.files[0]);
+  const uploadedFile = event.target.files[0];
+
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: "array" });
+
+    const sheetName = workbook.SheetNames[0]; // Get first sheet
+    const sheet = workbook.Sheets[sheetName];
+
+    taJsonData = XLSX.utils.sheet_to_json(sheet, {range: 6}); // Convert sheet to JSON
+    // console.log(jsonData);
+  };
+  reader.readAsArrayBuffer(uploadedFile); // Read the file
+});
+
+
+
+const taPetroCalculation = (e) =>{
+  console.log(e);
+
+}
+
+
+const taPetro = () => {
+  let newTaPetroData = [];
+
+  taJsonData.forEach((data) => {
+    console.log(data);
+    // const CalculatedData = taPetroCalculation(data);
+    // newTaPetroData.push(CalculatedData);
+  });
+}
+
+
+
   const downloadCSV = (processedData, fileName) => {
-    // console.log()
     const worksheet = XLSX.utils.json_to_sheet(processedData); // Convert JSON data to a worksheet
     const csvContent = XLSX.utils.sheet_to_csv(worksheet); // Convert worksheet to CSV format
     
@@ -79,10 +127,16 @@ document.addEventListener("DOMContentLoaded", () => {
     URL.revokeObjectURL(url);
   };
 
-  
+
+  //all btn click functionality
+  const taSubmitBtn = document.querySelector('#taSubmitBtn');
+
+
   submitBtn.addEventListener("click", ambest);
+  taSubmitBtn.addEventListener("click", taPetro);
+  
 });
 
 
-//ta petero
+
 
